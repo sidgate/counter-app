81c412cb750ee926fb5ac9bf7034c8c0603646ca
Siddharth Gargate <siddharth.gargate@springernature.com>
2019-06-14 16:33:24 +0530

    pass parameters to event

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export default class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value,
     tags: ["tag1", "tag2", "tag3"]
   };
 
@@ -36,7 +36,7 @@ export default class Counter extends Component {
           Increment
         </button>
         <button
-          onClick={this.props.onDelete}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -6,12 +6,16 @@ class Counters extends Component {
       { id: 1, value: 4 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
-      { id: 4, value: 0 }
+      { id: 4, value: 0 },
+      { id: 5, value: 0 }
     ]
   };
 
-  handleDelete = () => {
-    console.log("event handler");
+  handleDelete = counterId => {
+    console.log("event handler", counterId);
+    this.setState({
+      counters: this.state.counters.filter(c => c.id !== counterId)
+    });
   };
 
   render() {
@@ -20,8 +24,7 @@ class Counters extends Component {
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}
-            value={counter.value}
-            id={counter.id}
+            counter={counter}
             onDelete={this.handleDelete}
           >
             <h4> Counter # {counter.id}</h4>
