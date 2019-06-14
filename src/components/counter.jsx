1537dcfc4b4af8c61806e1f95ac6b807f935aa08
Siddharth Gargate <siddharth.gargate@springernature.com>
2019-06-14 15:14:38 +0530

    rendering on different conditions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,16 +6,25 @@ export default class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"]
   };
 
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags</p>;
+    return (
+      <ul>
+        {" "}
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}{" "}
+      </ul>
+    );
+  }
+
   render() {
     return (
       <React.Fragment>
         <span className={this.badgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
       </React.Fragment>
     );
   }
